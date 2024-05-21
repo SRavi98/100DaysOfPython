d288b6eaabfa9eb01faabb001f882f00cd4481bb
S Ravi <ravisiddapuram98@gmail.com>
2024-05-21 23:31:07 +0530

    Day-1 Band Name Generator [Modified]

---
+++ Day1.py
@@ -0,0 +1,12 @@
+#Band Name Generator Challenge (Modified Team Name Generator)
+
+print('Welcome to the Team Name Generator!')
+city_name = input("What's the name of the city you grew up in? \n")
+print(city_name)
+pet_name = input("What's the name of your pet? \n")
+print(pet_name)
+
+print('Your band name could be: ' 
+      + city_name.title() + " "+ pet_name.title() + " or "  + pet_name.title() + " " + city_name.title())
+
+print("Orange Army" + "\"Orange Army\"")
